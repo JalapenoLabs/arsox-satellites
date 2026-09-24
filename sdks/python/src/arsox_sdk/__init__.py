6004358c9fcecc80a04a573a7ed2dd78feb0c5b6
Alex Navarro <alex@navarrocity.com>
2026-09-24 16:05:24 -0600

    Expose thread services from the Node and Python SDKs

--- sdks/python/src/arsox_sdk/__init__.py
+++ sdks/python/src/arsox_sdk/__init__.py
@@ -74,7 +74,13 @@ from arsox_sdk.proto.arsox.satellite.v1.satellite_pb2 import (
     GetStatusResponse,
     GetVersionResponse,
 )
+from arsox_sdk.proto.arsox.settings.v1.service_pb2 import (
+    ReadinessProbe,
+    Service,
+    ServiceIsolation,
+)
 from arsox_sdk.proto.arsox.settings.v1.settings_pb2 import ThreadSettings
+from arsox_sdk.proto.arsox.settings.v1.tool_pb2 import McpServer, ServiceEndpoint
 from arsox_sdk.proto.arsox.thread.v1.thread_pb2 import (
     Thread,
     ThreadOrder,
@@ -122,10 +128,15 @@ __all__ = [
     "Incident",
     "IncidentCounts",
     "ListIncidentsRequest",
+    "McpServer",
     "Money",
     "PageRequest",
     "PageResponse",
+    "ReadinessProbe",
     "Secret",
+    "Service",
+    "ServiceEndpoint",
+    "ServiceIsolation",
     "Stage",
     "StageDisposition",
     "StageOutcome",
